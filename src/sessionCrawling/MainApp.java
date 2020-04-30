394a0cf3021886c5edcbed743be0ed96db91c0f9
ruthetum <ruthetum@gmail.com>
2020-04-30 15:42:09 +0900

    로그인해서 페이지 접속하기

--- src/sessionCrawling/MainApp.java
+++ src/sessionCrawling/MainApp.java
@@ -10,45 +10,23 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class MainApp {
-	private static final String LOGIN_URL = "https://everytime.kr/login";
+	private static final String LOGIN_URL = "https://everytime.kr/user/login";
 	private static final String URL = "https://everytime.kr/";
-	private static String ID = "아이디 입력하기";
-	private static String PW = "비밀번호 입력하기";
+	private static String ID = "아이디를 입력해주세연";
+	private static String PW = "비번을 입력해주세연";
 	
 	public static void main(String[] args) throws IOException {
-		Connection.Response loginForm = Jsoup.connect(LOGIN_URL)
-				.method(Connection.Method.GET)
-				.execute();
-		
-		Connection.Response mainPage = Jsoup.connect(LOGIN_URL)
+		// 1. login
+		Response loginResponse = (Response) Jsoup.connect(LOGIN_URL)
 				.data("userid", ID)
 				.data("password", PW)
-				.data("redirect", "/")
-				.cookies(loginForm.cookies())
-				.followRedirects(true)
-				.method(Connection.Method.POST)
+				.data("autologin", "1")
+				.method(Method.POST)
 				.execute();
 		
-		Map<String, String> cookies = mainPage.cookies();
-		
-//		Document doc = Jsoup.connect(URL).cookies(cookies).get();
-		Document doc = Jsoup.connect(URL).cookies(cookies).execute().parse();
-		
-		System.out.println(doc);
-		
-		
-		
-		// 1. login
-//		Response loginResponse = (Response) Jsoup.connect(LOGIN_URL)
-//				.data("userid", ID)
-//				.data("password", PW)
-//				.data("autologin", "1")
-//				.method(Method.POST)
-//				.execute();
-//		
-//		System.out.println(" - PAGE STATUS CODE : " + loginResponse.statusCode());
-//		Document doc = loginResponse.parse();
-//		System.out.println("" + doc.toString());
+		System.out.println(" - PAGE STATUS CODE : " + loginResponse.statusCode());
+		Document doc = loginResponse.parse();
+		System.out.println("" + doc.toString());
 		
 		// 2. Session 정보 담기
 		
